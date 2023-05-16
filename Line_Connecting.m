779d321f1f49444696c3bed343ed71ff958e21e8
حسين مصطفي محمد صلاح الدين <14712020101885@stud.cu.edu.eg>
2023-05-16 16:22:49 +0300

    Line_Drawing

---
+++ Line_Connecting.m
@@ -0,0 +1,16 @@
+function can = Line_Connecting(X1, Y1, X2 , Y2, phi, L1, L2, L3,Theta1min,Theta1max,Theta2min,Theta2max,Theta3min,Theta3max)
+can = 1;
+RangeX=X1-X2;
+RangeY=Y1-Y2;
+
+for i = 0:100 
+    can=(Point_Check(X2+RangeX*i/100,Y2+RangeY*i/100,Theta1min,Theta1max,Theta2min,Theta2max,Theta3min,Theta3max));
+    if(can==0)
+        break;
+    end
+end
+if(can==1)
+    for i = 0:100 
+    ikpm(X2+RangeX*i/100,Y2+RangeY*i/100, phi, L1, L2, L3);
+    end
+end
