0ee0c5557b4e0f226573247d3a10db28a1595d13
حسين مصطفي محمد صلاح الدين <14712020101885@stud.cu.edu.eg>
2023-05-16 16:25:56 +0300

    Fixed Problem With End

--- Line_Connecting.m
+++ Line_Connecting.m
@@ -1,4 +1,4 @@
-function can = Line_Connecting(X1, Y1, X2 , Y2, phi, L1, L2, L3,Theta1min,Theta1max,Theta2min,Theta2max,Theta3min,Theta3max)
+function can = Line_connecting(X1, Y1, X2 , Y2, phi, L1, L2, L3,Theta1min,Theta1max,Theta2min,Theta2max,Theta3min,Theta3max)
 can = 1;
 RangeX=X1-X2;
 RangeY=Y1-Y2;
@@ -13,4 +13,5 @@ if(can==1)
     for i = 0:100 
     ikpm(X2+RangeX*i/100,Y2+RangeY*i/100, phi, L1, L2, L3);
     end
+end
 end
